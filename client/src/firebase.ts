17a5846f2d41ed7ede9031dde552b9608df0c978
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-07-23 16:24:23 -0400

    feat(client): ✨ read database for product information
    Co-authored-by: Chiara <coxender@github.com>

---
+++ client/src/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { CollectionReference, getFirestore } from "firebase/firestore";
+import { collection } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlCorjwKWeIT27bfClJljDGLVv__9PaRw",
+  authDomain: "hedge-site.firebaseapp.com",
+  projectId: "hedge-site",
+  storageBucket: "hedge-site.appspot.com",
+  messagingSenderId: "662700376854",
+  appId: "1:662700376854:web:83529af948da8555dcc637",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export interface Product {
+  name: string;
+  description: string;
+  price: number;
+  qty: number;
+  sellerUID: string;
+}
+
+export const productCollection = collection(db, "products") as CollectionReference<Product>;
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,63 +1,36 @@
 import { render, html } from "lit";
-
-export interface Products {
-  name: string;
-  description: string;
-  price: number;
-  qty: number;
-  sellerUID: string;
-}
-
-const products: Products[] = [
-  {
-    name: "Joe's Product",
-    description: "lorem ipsum",
-    price: 30.0,
-    qty: 2,
-    sellerUID: "saidygaiasydd",
-  },
-  {
-    name: "Toy 1",
-    description: "this is a toy",
-    price: 0.3,
-    qty: 30,
-    sellerUID: "saidygaiasyd",
-  },
-  {
-    name: "Toy 2",
-    description: "this is a toy",
-    price: 0.3,
-    qty: 30,
-    sellerUID: "saidygaiasyd",
-  },
-  {
-    name: "Toy 3",
-    description: "this is a toy",
-    price: 0.3,
-    qty: 30,
-    sellerUID: "saidygaiasyd",
-  },
-];
+import { Product, productCollection } from "./firebase";
+import { getDocs } from "firebase/firestore";
 
 const salesContainer = document.querySelector<HTMLDivElement>(".sales-container");
-if (salesContainer == null) {
-  throw new Error("sales-container does not exist.");
+
+async function updateProducts() {
+  const querySnapshot = await getDocs(productCollection);
+
+  const template = querySnapshot.docs.map((doc) => {
+    const product: Product = doc.data();
+
+    return html`<div class="sales-card">
+      <div class="sales-card-header">
+        <span>${product.name}</span>
+        <span>$${product.price}</span>
+        <span>Qty: ${product.qty}</span>
+        <span>Offers: 1</span>
+      </div>
+      <div class="sales-card-body">
+        <span> Description:</span>
+        <div>${product.description}</div>
+      </div>
+    </div>`;
+  });
+
+  if (salesContainer == null) {
+    throw new Error("sales-container does not exist.");
+  }
+
+  render(template, salesContainer);
 }
 
-const template = products.map(
-  (product) => html` <div class="sales-card">
-    <div class="sales-card-header">
-      <span>${product.name}</span>
-      <span>$${product.price}</span>
-      <span>Qty: ${product.qty}</span>
-      <span>Offers: 1</span>
-    </div>
-    <div class="sales-card-body">
-      <span> Description:</span>
-      <div>${product.description}</div>
-    </div>
-  </div>`
-);
+updateProducts();
 
-render(template, salesContainer);
 // TODO the same thing with buy but omit Offers
