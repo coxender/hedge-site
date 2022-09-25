601d721d1276cd83c91a5be438c92de375152e75
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-09-25 15:14:32 -0400

    refactor(client): ♻️ split firebase into multiple files

---
+++ client/src/firebase/app.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlCorjwKWeIT27bfClJljDGLVv__9PaRw",
+  authDomain: "hedge-site.firebaseapp.com",
+  projectId: "hedge-site",
+  storageBucket: "hedge-site.appspot.com",
+  messagingSenderId: "662700376854",
+  appId: "1:662700376854:web:83529af948da8555dcc637",
+};
+
+export const app = initializeApp(firebaseConfig);
