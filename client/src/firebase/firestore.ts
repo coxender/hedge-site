eecb57b2287ae7a34367f1ec55c314adeea1689d
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-11-26 16:59:45 -0500

    docs(client): :memo: Add docs to Product type
    Co-authored-by:  Nate <nstringham@users.noreply.github.com>

--- client/src/firebase/firestore.ts
+++ client/src/firebase/firestore.ts
@@ -5,10 +5,15 @@ import { app } from "./app";
 const db = getFirestore(app);
 
 export interface Product {
-  name: string;
+  /** name of product max length 60 characters. */
+  name: string; 
+  /** description of product max length 1000 */
   description: string;
+  /** unit price of product, nonegative integer*/
   price: number;
+  /** quantity availible of product, nonegative integer */
   qty: number;
+  /** Firebase User Uid of the current user */
   sellerUid: string;
 }
 
