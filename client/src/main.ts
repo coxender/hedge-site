e6d7bdcce74dd16c0b30c02f4bad3a589166cf81
Chiara Oxender <46496608+coxender@users.noreply.github.com>
2022-11-26 16:45:14 -0500

    feat(client): :sparkles: add products for user
    a user can now add products and have them saved to firestore and shown on page.
    
    Co-authored-by: The smart one <nstringham@users.noreply.github.com>

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,58 @@
 import { render, html } from "lit";
 import { Product, productCollection } from "./firebase/firestore";
-import { getDocs, orderBy, query, where } from "firebase/firestore";
+import { getDocs, orderBy, query, where, addDoc } from "firebase/firestore";
 import { subscribeToUser } from "./firebase/auth";
 
 let userId: string | undefined = undefined;
 
 const userContainer = document.querySelector<HTMLDivElement>(".user-container");
+const addProductButton = document.querySelector<HTMLButtonElement>("#add-product-button");
+const productForm = document.querySelector(".product-form") as HTMLFormElement;
+
+addProductButton?.addEventListener("click", () => {
+  productForm.classList.toggle("hidden");
+});
+
+productForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formInfo = new FormData(productForm);
+
+  const name = formInfo.get("name");
+  if (typeof name != "string") {
+    throw new Error("name was not a string");
+  }
+
+  const description = formInfo.get("desc");
+  if (typeof description != "string") {
+    throw new Error("description was not a string");
+  }
+
+  const price = formInfo.get("price");
+  if (typeof price != "string") {
+    throw new Error("price was not a string");
+  }
+
+  const quantity = formInfo.get("qty");
+  if (typeof quantity != "string") {
+    throw new Error("quantity was not a string");
+  }
+
+  if (userId == undefined) {
+    throw new Error("userId was undefined");
+  }
+
+  const product: Product = {
+    name,
+    description,
+    price: parseInt(price),
+    qty: parseInt(quantity),
+    sellerUid: userId,
+  };
+  addDoc(productCollection, product);
+
+  productForm.reset();
+  updateUserProducts();
+});
 
 async function updateUserProducts() {
   const userProductQuery = query(productCollection, where("sellerUid", "==", userId ?? ""), orderBy("price", "desc"));
