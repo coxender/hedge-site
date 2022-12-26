e0a298e7fb634c3f2c148ae6b8471740ab49b643
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2022-12-26 18:05:20 -0500

    feat(client): ✨ add a subscription to make products update
    Co-authored-by: Chiara Oxender <46496608+coxender@users.noreply.github.com>

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,3 +1,4 @@
+import "./other-products";
 import { render, html } from "lit";
 import { Product, productCollection } from "./firebase/firestore";
 import { getDocs, orderBy, query, where, addDoc } from "firebase/firestore";
@@ -81,50 +82,16 @@ async function updateUserProducts() {
   render(template, userContainer);
 }
 
-const otherContainer = document.querySelector<HTMLDivElement>(".other-container");
-
-async function updateOtherProducts() {
-  const otherProductsQuery = query(
-    productCollection,
-    orderBy("sellerUid"),
-    where("sellerUid", "!=", userId ?? ""),
-    orderBy("price", "desc")
-  );
-  const querySnapshot = await getDocs(otherProductsQuery);
-  const template = querySnapshot.docs.map((doc) => {
-    const product: Product = doc.data();
-
-    return html`<div class="other-card">
-      <div class="other-card-header">
-        <span>${product.name}</span>
-        <span>$${product.price}</span>
-        <span>Qty: ${product.qty}</span>
-        <span>Seller: ${product.sellerUid}</span>
-      </div>
-      <div class="other-card-body">
-        <span> Description:</span>
-        <div>${product.description}</div>
-      </div>
-    </div>`;
-  });
-
-  if (otherContainer == null) {
-    throw new Error("user-container does not exist.");
-  }
-
-  render(template, otherContainer);
-}
-
 subscribeToUser((user) => {
   userId = user?.uid;
   updateUserProducts();
-  updateOtherProducts();
   if (user != null) {
     enableButtons();
   } else {
     disableButtons();
   }
 });
+
 function enableButtons() {
   const restrictedButtons = document.querySelectorAll<HTMLButtonElement>(".login-only");
   for (const button of restrictedButtons) {
---
+++ client/src/other-products.ts
@@ -0,0 +1,42 @@
+import { render, html } from "lit";
+import { Product, productCollection } from "./firebase/firestore";
+import { onSnapshot, orderBy, query, where } from "firebase/firestore";
+import { getCurrentUser, subscribeToUser } from "./firebase/auth";
+
+const otherContainer = document.querySelector<HTMLDivElement>(".other-container");
+
+let unsubscribe: () => void;
+
+subscribeToUser((user) => {
+  unsubscribe?.();
+  const otherProductsQuery = query(
+    productCollection,
+    orderBy("sellerUid"),
+    where("sellerUid", "!=", user?.uid ?? ""),
+    orderBy("price", "desc")
+  );
+  unsubscribe = onSnapshot(otherProductsQuery, (querySnapshot) => {
+    const template = querySnapshot.docs.map((doc) => {
+      const product: Product = doc.data();
+
+      return html`<div class="other-card">
+        <div class="other-card-header">
+          <span>${product.name}</span>
+          <span>$${product.price}</span>
+          <span>Qty: ${product.qty}</span>
+          <span>Seller: ${product.sellerUid}</span>
+        </div>
+        <div class="other-card-body">
+          <span> Description:</span>
+          <div>${product.description}</div>
+        </div>
+      </div>`;
+    });
+
+    if (otherContainer == null) {
+      throw new Error("user-container does not exist.");
+    }
+
+    render(template, otherContainer);
+  });
+});
